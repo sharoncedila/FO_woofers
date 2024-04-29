a69f4280082e2479a5383dedc6ec09a0206e8b85
felesia <felesialo12@gmail.com>
2024-04-30 04:37:47 +0700

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:woofers/pages/add_dog_page.dart';
 import 'package:woofers/pages/login_page.dart';
 
 void main() {
@@ -14,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AddDogPage(),
+      home: LoginPage(),
     );
   }
 }
