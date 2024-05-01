728d111771ca4dc36e8f7d9648b058620a5045fa
felesia <felesialo12@gmail.com>
2024-05-01 13:14:32 +0700

    no message

--- lib/classes/dio_instance.dart
+++ lib/classes/dio_instance.dart
@@ -8,10 +8,11 @@ class DioInstance {
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString('accessToken');
     instance ??= Dio(BaseOptions(
-      baseUrl: 'http://192.168.249.53:8080/woofers',
+      baseUrl: 'http://192.168.68.172:8080/woofers',
       //192.168.182.165
       //192.168.100.8
       //192.168.249.53
+      //192.168.68.172
       headers: Map.from({"Authorization": "Bearer $token"}),
     ));
 
