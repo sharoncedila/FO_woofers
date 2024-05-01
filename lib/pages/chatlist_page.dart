fb8697b016e68aac7fffc629edd422254418b0b2
felesia <felesialo12@gmail.com>
2024-05-01 10:39:02 +0700

    no message

--- lib/pages/chatlist_page.dart
+++ lib/pages/chatlist_page.dart
@@ -4,6 +4,7 @@ import 'package:hexcolor/hexcolor.dart';
 import 'package:woofers/components/chat_card.dart';
 import 'package:woofers/pages/notification_page.dart';
 import 'package:woofers/services/chat/chat_service.dart';
+
 class ChatListPage extends StatefulWidget {
   const ChatListPage({super.key});
 
@@ -12,122 +13,131 @@ class ChatListPage extends StatefulWidget {
 }
 
 class _ChatListPageState extends State<ChatListPage> {
+  final TextEditingController _searchController = TextEditingController();
+// late Future<List<ChatlistDetail>> _chatroomListFuture;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _chatroomListFuture = ChatroomService().retrieveChatroomList();
+//   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar:
-        AppBar(
-          toolbarHeight: 75, 
-          elevation: 0,
-          backgroundColor: HexColor("#a0dcdc"),
-          title:
-            Text(
-              "CHATS",
-              style: GoogleFonts.lora(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: const Color.fromRGBO(40,36,36,10000),
-              ),
-            ),
-            actions: <Widget>[
-              IconButton(
-                icon: const Icon(Icons.notification_add_rounded),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const NotificationPage()),
-                  );
-                },
-              ),
-              const SizedBox(width: 5,)
-            ],
+      appBar: AppBar(
+        toolbarHeight: 75,
+        elevation: 0,
+        backgroundColor: HexColor("#a0dcdc"),
+        title: Text(
+          "CHATS",
+          style: GoogleFonts.lora(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            color: const Color.fromRGBO(40, 36, 36, 10000),
           ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.notification_add_rounded),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const NotificationPage()),
+              );
+            },
+          ),
+          const SizedBox(
+            width: 5,
+          )
+        ],
+      ),
       body: ChatList(),
     );
   }
+
   Widget ChatList() {
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.only(left: 25, right: 25),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 15,
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            // search box
-            Container(
-              width: double.infinity, 
-              height: 48,
-              decoration: BoxDecoration(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          const SizedBox(
+            height: 15,
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          // search box
+          Container(
+            width: double.infinity,
+            height: 48,
+            decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(15),
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.grey.withOpacity(0.2),
-                    spreadRadius: 2,
-                    blurRadius: 15,
-                    offset: const Offset(0, 1)
-                  )
-                ]
-              ),
-              child: Row(
-                children: [
-                  const SizedBox(
-                    width: 5,
+                      color: Colors.grey.withOpacity(0.2),
+                      spreadRadius: 2,
+                      blurRadius: 15,
+                      offset: const Offset(0, 1))
+                ]),
+            child: Row(
+              children: [
+                const SizedBox(
+                  width: 5,
+                ),
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.search,
+                      color: Colors.black,
+                    )),
+                const SizedBox(
+                  width: 5,
+                ),
+                Flexible(
+                  child: TextField(
+                    controller: _searchController,
+                    cursorColor: Colors.black,
+                    decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search for others"),
+                    onChanged: (value) {
+                      setState(() {});
+                    },
                   ),
-                  IconButton( 
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.search,
-                        color: Colors.black,
-                      )),
-                  const SizedBox(
-                    width: 5,
-                  ),
-                  const Flexible(
-                    child: TextField(
-                      cursorColor: Colors.black,
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Search for others"),
-                    ),
-                  )
-                ],
-              ),
+                )
+              ],
             ),
-            Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-          child: FutureBuilder(
-            future: ChatroomService().retrieveChatroomList(),
-            builder: ((context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: Text("Retrieving your data..."));
-              }
-              if (snapshot.hasError) {
-                return const Center(child: Text("Error chatlist"));
-              }
-              if (!snapshot.hasData) {
-                return const Text("No data");
-              }
-              final chatroomResponse = snapshot.data!;
-              final chatrooms;
-              
-              return Wrap(
-                children: 
-                chatrooms = chatroomResponse.chatlistList
-                    .map((e) => ChatCardDetail(chatlistDetail: e))
-                    .toList(),
-              );
-            }),
           ),
-        ),
-          ]
-        ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+            child: FutureBuilder(
+              future: ChatroomService().retrieveChatroomList(),
+              builder: ((context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: Text("Retrieving your data..."));
+                }
+                if (snapshot.hasError) {
+                  return const Center(child: Text("Error chatlist"));
+                }
+                if (!snapshot.hasData) {
+                  return const Text("No data");
+                }
+                final chatroomResponse = snapshot.data!;
+                final List<ChatCardDetail> chatrooms;
+
+                return Wrap(
+                  children: chatrooms = chatroomResponse.chatlistList
+                      .map((e) => ChatCardDetail(chatlistDetail: e))
+                      .toList(),
+                );
+              }),
+            ),
+          ),
+        ]),
       ),
     );
   }
