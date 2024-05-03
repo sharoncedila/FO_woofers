eb3fc7e2e55529c86375ff98ba2a3ebf64d4f902
felesia <felesialo12@gmail.com>
2024-05-03 19:55:26 +0700

    no message

--- lib/pages/add_feeds_page.dart
+++ lib/pages/add_feeds_page.dart
@@ -18,23 +18,21 @@ class _AddFeedsPageState extends State<AddFeedsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar:
-        AppBar(
-          toolbarHeight: 75,
-          elevation: 0,
-          backgroundColor: HexColor("#a0dcdc"),
-          centerTitle: true,
-          title:
-            Text(
-              "POST NEW FEED",
-              style: GoogleFonts.lora(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: const Color.fromRGBO(40,36,36,10000),
-              ),
-              textAlign: TextAlign.center,
-            ),
+      appBar: AppBar(
+        toolbarHeight: 75,
+        elevation: 0,
+        backgroundColor: HexColor("#a0dcdc"),
+        centerTitle: true,
+        title: Text(
+          "POST NEW FEED",
+          style: GoogleFonts.lora(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            color: const Color.fromRGBO(40, 36, 36, 10000),
           ),
+          textAlign: TextAlign.center,
+        ),
+      ),
       body: addFeeds(),
     );
   }
@@ -42,12 +40,17 @@ class _AddFeedsPageState extends State<AddFeedsPage> {
   Widget addFeeds() {
     return Column(
       children: [
-        const SizedBox(height: 15,),
+        const SizedBox(
+          height: 15,
+        ),
         const Image(image: AssetImage('assets/dog_picture/dog1.jpg')),
-        const SizedBox(height: 15,),
+        const SizedBox(
+          height: 15,
+        ),
         Padding(
           padding: const EdgeInsets.only(left: 25, right: 25),
-          child:  TextField(
+          child: TextField(
+            maxLength: 500,
             controller: _captionController,
             decoration: const InputDecoration(
               labelText: 'Enter your captions..',
@@ -59,7 +62,9 @@ class _AddFeedsPageState extends State<AddFeedsPage> {
             maxLines: null,
           ),
         ),
-        const SizedBox(height: 15,),
+        const SizedBox(
+          height: 15,
+        ),
         SizedBox(
           width: MediaQuery.of(context).size.width / 1.1,
           height: 45,
@@ -68,23 +73,28 @@ class _AddFeedsPageState extends State<AddFeedsPage> {
               backgroundColor: MaterialStateProperty.resolveWith(
                 (Set<MaterialState> states) {
                   if (states.contains(MaterialState.pressed)) {
-                    return Theme.of(context).colorScheme.primary.withOpacity(0.5);
+                    return Theme.of(context)
+                        .colorScheme
+                        .primary
+                        .withOpacity(0.5);
                   }
                   return null; // Use the component's default.
                 },
               ),
             ),
-          child: const Text('POST'),
-          onPressed: () {
-            final PostFeedsRequest request = PostFeedsRequest(
-              caption: _captionController.text,
-              image: null,
-            );
+            child: const Text('POST'),
+            onPressed: () {
+              final PostFeedsRequest request = PostFeedsRequest(
+                caption: _captionController.text,
+                image: null,
+              );
 
-            FeedsService().postFeeds(request).then((value) => Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => const FeedsPage())));
-          },
+              FeedsService().postFeeds(request).then((value) =>
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (_) => const FeedsPage())));
+            },
 
-          /*
+            /*
           oonPressed: () {
             print(_isLike);
             setState(() {
@@ -95,8 +105,8 @@ class _AddFeedsPageState extends State<AddFeedsPage> {
             });
           },
           */
+          ),
         ),
-      ),
       ],
     );
   }
