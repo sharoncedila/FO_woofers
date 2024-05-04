b9eed2697ec55024700a873a3c02695944007529
sharoncedila <shastep1927@gmail.com>
2024-05-04 18:00:13 +0700

    no message

--- lib/classes/ws_chat_instance.dart
+++ lib/classes/ws_chat_instance.dart
@@ -17,7 +17,7 @@ class WSChatInstance {
         HttpHeaders.authorizationHeader: 'Bearer $token',
       };
       session = await WebSocket.connect(
-          'ws://192.168.100.249:8080/woofers/send-chat',
+          'ws://10.88.20.63:8080/woofers/send-chat',
           headers: headers);
       print("Successfully Connect to WebSocket Chat Server");
 
--- lib/classes/ws_notif_instance.dart
+++ lib/classes/ws_notif_instance.dart
@@ -16,7 +16,7 @@ class WSNotifInstance {
         HttpHeaders.authorizationHeader: 'Bearer $token',
       };
       session = await WebSocket.connect(
-          'ws://192.168.100.249:8080/woofers/app-notif',
+          'ws://10.88.20.63:8080/woofers/app-notif',
           headers: headers);
       print("Successfully Connect to WebSocket Notif Server");
 
