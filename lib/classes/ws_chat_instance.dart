8f2a3c62e3318055c24415883a8dc3a9d4b8d16a
vincentlee23 <vincentlee.vk23@gmail.com>
2024-05-05 16:27:31 +0700

    enhance image service

--- lib/classes/ws_chat_instance.dart
+++ lib/classes/ws_chat_instance.dart
@@ -16,7 +16,7 @@ class WSChatInstance {
         HttpHeaders.authorizationHeader: 'Bearer $token',
       };
       session = await WebSocket.connect(
-          'ws://10.88.20.142:8080/woofers/send-chat',
+          'ws://192.168.68.174:8080/woofers/send-chat',
           headers: headers);
       print("Successfully Connect to WebSocket Chat Server");
 
