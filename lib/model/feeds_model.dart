289446407bb54d64da47d64bbe342fc129bb0b7f
felesia <felesialo12@gmail.com>
2024-05-08 09:22:05 +0700

    no message

--- lib/model/feeds_model.dart
+++ lib/model/feeds_model.dart
@@ -16,7 +16,6 @@ class LeaveCommentRequest with _$LeaveCommentRequest {
 class OpenCommentsResponse with _$OpenCommentsResponse{
   factory OpenCommentsResponse({
     required String? username,
-    // required String? commentId,
     required String? feedsId,
     required String? accountId,
     required String? content,
@@ -61,11 +60,6 @@ class ViewFeedsResponse with _$ViewFeedsResponse{
 @freezed
 class LeaveCommentResponse with _$LeaveCommentResponse {
   factory LeaveCommentResponse({
-    // required String commentId,
-    // required String feedsId,
-    // required String accountId,
-    // required String content,
-    // required String commentDate
     required String? errorCode,
     required String? errorMessage
   }) = _LeaveCommentResponse;
@@ -76,15 +70,6 @@ class LeaveCommentResponse with _$LeaveCommentResponse {
 @freezed
 class PostFeedsResponse with _$PostFeedsResponse{
   factory PostFeedsResponse({
-    // required String? feedsId,
-    // required String? accountId,
-    // required String? username,
-    // required String? likesCount,
-    // required String? caption,
-    // required String? image,
-    // required String? reportCount,
-    // required String? postDate,
-    // required String? deletedAt,
     required String? errorCode,
     required String? errorMessage
   }) = _PostFeedsResponse;
